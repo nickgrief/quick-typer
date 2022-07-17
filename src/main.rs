d64170476e6d660a7627cb146d49466e0500b0c4
nickgrief <wotniga@gmail.com>
2022-07-17 11:18:57 +0300

    Add read file function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+
 fn main() {
-    println!("Hello, world!");
+    let path = "./assets/dict.txt";
+    let mut words = vec![];
+    read_lines(path, &mut words).unwrap();
+    println!("{:?}", words);
+}
+
+fn read_lines(path: &str, output: &mut Vec<String>) -> Result<(), Error> {
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    for line in buffered.lines() {
+        output.push(line?);
+    }
+
+    Ok(())
 }
